2e6a499687d5a75f31b5e712875f9c35911bc2a8
roksana-rachel <roksana.rachel@gmail.com>
2024-02-06 18:07:51 +0100

    minor fix

--- src/app/email/route.ts
+++ src/app/email/route.ts
@@ -1,13 +1,11 @@
 import { sendEmail } from '@/server/controllers/emailController';
-import { stat } from 'fs';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
     const data = await request.json();
-    const data2 = { ...data, from: 'abc' };
     let res;
     try {
-        const response = await sendEmail(data2);
+        const response = await sendEmail(data);
         res = response[0];
     } catch (error) {
         res = { error: 'Internal Server Error', status: 500 };
