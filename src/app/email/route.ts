a7f4bd0e693ef97e869894991632910df12b391b
roksana-rachel <roksana.rachel@gmail.com>
2024-02-13 17:58:31 +0100

    response fix

--- src/app/email/route.ts
+++ src/app/email/route.ts
@@ -3,13 +3,13 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
     const data = await request.json();
-    let res;
     try {
         const response = await sendEmail(data);
-        res = response[0];
+        return NextResponse.json(response[0]);
     } catch (error) {
-        res = { error: 'Internal Server Error', status: 500 };
+        return NextResponse.json(
+            { error: 'Internal Server Error' },
+            { status: 500 },
+        );
     }
-
-    return NextResponse.json(res);
 }
