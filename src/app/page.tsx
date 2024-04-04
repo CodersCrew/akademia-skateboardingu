7a8bc26ef46356796f52ef3ef52f0398dd5523c7
T0ffik <t0fffik2000@gmail.com>
2024-04-04 12:17:23 +0200

    create structure of welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,41 @@
-const AcademyPage = () => <div>Akademia</div>;
+import AboutUs from '../components/AboutUs/AboutUs';
+import Banner from '../components/Banner/Banner';
+import Contact from '../components/Contact/Contact';
+import FAQ from '../components/FAQ/FAQ';
+import Footer from '../components/Footer/Footer';
+import Navbar from '../components/navbar/Navbar';
+
+const items = [
+  {
+    src: 'images/skate_cut.png',
+    key: 'baner1',
+    message: 'Poznajmy się!'
+  },
+  {
+    src: 'images/skate_cut.png',
+    key: 'baner2',
+    message: 'bądźmy',
+    subMessage: 'w kontakcie!'
+  },
+  {
+    src: 'images/skate_cut.png',
+    key: 'baner3',
+    message: 'inni',
+    subMessage: 'pytali o...'
+  }
+];
+
+const AcademyPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <Banner items={items} />
+      <AboutUs />
+      <Contact />
+      <FAQ />
+      <Footer />
+    </div>
+  );
+};
 
 export default AcademyPage;
