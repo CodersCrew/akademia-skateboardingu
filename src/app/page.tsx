ae5285f6b5f465b040efc35d9f84801942404950
T0ffik <t0fffik2000@gmail.com>
2024-04-05 11:54:45 +0200

    correct structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,5 @@
-import AboutUs from '../components/AboutUs/AboutUs';
-import Banner from '../components/Banner/Banner';
-import Contact from '../components/Contact/Contact';
-import FAQ from '../components/FAQ/FAQ';
-import Footer from '../components/Footer/Footer';
-import Navbar from '../components/navbar/Navbar';
-
-const items = [
-  {
-    src: 'images/skate_cut.png',
-    key: 'baner1',
-    message: 'Poznajmy się!'
-  },
-  {
-    src: 'images/skate_cut.png',
-    key: 'baner2',
-    message: 'bądźmy',
-    subMessage: 'w kontakcie!'
-  },
-  {
-    src: 'images/skate_cut.png',
-    key: 'baner3',
-    message: 'inni',
-    subMessage: 'pytali o...'
-  }
-];
-
 const AcademyPage = () => {
-  return (
-    <div>
-      <Navbar />
-      <Banner items={items} />
-      <AboutUs />
-      <Contact />
-      <FAQ />
-      <Footer />
-    </div>
-  );
+  return <div>Akademia</div>;
 };
 
 export default AcademyPage;
--- src/components/ContactSection/ContactSection.tsx
+++ src/components/ContactSection/ContactSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Contact = () => {
+const ContactSection = () => {
   return <section>Contact</section>;
 };
 
-export default Contact;
+export default ContactSection;
