f30b9d68fde12bae7a617ab483320fb9650ce510
AnnaRaczy <38297862+AnnaRaczy@users.noreply.github.com>
2024-04-09 18:35:15 +0200

    mapping fix

--- src/components/main/Sets/Sets.tsx
+++ src/components/main/Sets/Sets.tsx
@@ -1,18 +1,35 @@
+import Image, { StaticImageData } from 'next/image';
+import Link from 'next/link';
+
 import photoMain_1 from '/public/images/photo_Main_1.png';
 import photoMain_2 from '/public/images/photo_Main_2.png';
 
 import Title from '../Common/Title';
-import Buttons from './Buttons';
+import Description from './Description';
+
+type ButtonTypes = {
+  image: StaticImageData;
+  path: string;
+  text: string;
+};
 
 const Sets = () => {
-  const images = [photoMain_1, photoMain_2];
-  const paths = ['/sets/beginner', '/sets/advanced'];
-  const text = ['POCZĄTKUJĄCY', 'ZAAWANSOWANI'];
+  const photosData: ButtonTypes[] = [
+    { image: photoMain_1, path: '/sets/beginner', text: 'POCZĄTKUJĄCY' },
+    { image: photoMain_2, path: '/sets/advanced', text: 'ZAAWANSOWANI' }
+  ];
 
   return (
     <div className="h-567">
       <Title title="KOMPLETY:" />
-      <Buttons images={images} paths={paths} text={text} />
+      <div className="h-489 mt-20 flex items-center justify-center space-x-40">
+        {Object.values(photosData).map((element, index) => (
+          <Link key={element.image.blurDataURL} href={element.path}>
+            <Image src={element.image} alt={`photo-${index + 1}`} />
+            <Description text={element.text} />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
