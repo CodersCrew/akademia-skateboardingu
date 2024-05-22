b4dfc562fd5a6298d30a433aca8d3e54f1f77a51
roksana-rachel <roksana.rachel@gmail.com>
2024-05-22 09:08:28 +0200

    added breadcrumbs

---
+++ src/components/Breadcrumbs.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const Breadcrumbs = () => {
+  const currentPath = usePathname();
+  const pathnames = currentPath.split('/').filter(x => x);
+  let breadcrumbPath = '';
+
+  return (
+    <ul className="flex flex-row">
+      <li key="home">
+        <Link href="/">Strona główna</Link>
+      </li>
+
+      {pathnames.map(path => {
+        breadcrumbPath += `/${path}`;
+
+        return (
+          <li className="before:px-3 before:content-['\3E']">
+            <Link href={breadcrumbPath} key={path}>
+              {path}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Breadcrumbs;
