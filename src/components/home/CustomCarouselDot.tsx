2e7a432a60395fcfde97f97ff9ffe8910a3de11a
Jedrzej Ratajczak <jratajczak@splashbrands.com>
2024-09-14 22:14:42 +0200

    Review

--- src/components/home/CustomCarouselDot.tsx
+++ src/components/home/CustomCarouselDot.tsx
@@ -2,7 +2,7 @@ import { DotProps } from 'react-multi-carousel';
 
 const CustomCarouselDot = ({ index, onClick, active }: DotProps) => (
   <li
-    className={`mx-0.5 h-6 w-6 rounded-full border-4 border-white ${!active ? 'cursor-pointer' : ''}`}
+    className={`mx-0.5 h-6 w-6 rounded-full border-4 border-white ${active ? '' : 'cursor-pointer'}`}
     onClick={onClick ? () => onClick() : undefined}
   ></li>
 );
