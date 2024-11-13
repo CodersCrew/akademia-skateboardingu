4a3b998cb85679a2d972a8f7dae73336c8aad332
Jedrzej Ratajczak <jratajczak@splashbrands.com>
2024-11-14 00:03:06 +0100

    Refactor

--- src/components/home/sections/CommentsSection.tsx
+++ src/components/home/sections/CommentsSection.tsx
@@ -4,87 +4,66 @@ import 'react-multi-carousel/lib/styles.css';
 import Image from 'next/image';
 import Carousel from 'react-multi-carousel';
 
-import CustomCarouselDot from '../CustomCarouselDot';
+import SectionContainer from '../SectionContainer';
 import { Title } from '../Title';
 import { TripleExclamationMark } from '../TripleExclamationMark';
 
-const comments = [
-  { id: 1, src: '/images/comment.png' },
-  { id: 2, src: '/images/comment.png' },
-  { id: 3, src: '/images/comment.png' },
-  { id: 4, src: '/images/comment.png' },
-  { id: 5, src: '/images/comment.png' },
-  { id: 6, src: '/images/comment.png' },
-  { id: 7, src: '/images/comment.png' }
+const commentPairs = [
+  ['/images/comment.png', '/images/comment.png'],
+  ['/images/comment.png', '/images/comment.png'],
+  ['/images/comment.png', '/images/comment.png'],
+  ['/images/comment.png']
 ];
-
-// TODO subject to change according to design
-const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 1
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 1
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
-};
-
-const splitArrayIntoChunks = (
-  array: { id: number; src: string }[],
-  size: number
-) =>
-  Array.from({ length: Math.ceil(array.length / size) }, (_element, index) =>
-    array.slice(index * size, index * size + size)
-  );
-
-const COMMENTS_PER_PAGE = 6;
 
 export function CommentsSection() {
   return (
-    <div className="flex flex-col p-5">
+    <SectionContainer className="flex flex-col">
       <div className="relative">
-        <Title title="Poznaj nasze opinie" titleClassName="text-[#88DBB6]" />
-        <TripleExclamationMark className="left-[calc(50%+200px)] top-1 md:left-[calc(50%+260px)]" />
+        <Title
+          title="Poznaj nasze opinie"
+          titleClassName="text-[#88DBB6] md:text-[3.8rem] lg:text-[4.5rem] xl:text-[5rem]"
+        />
+        <TripleExclamationMark className="-top-[12px] left-[calc(50%+110px)] min-[371px]:left-[calc(50%+170px)] md:-top-[30px] md:left-[calc(50%+280px)] lg:left-[calc(50%+340px)] xl:left-[calc(50%+380px)]" />
       </div>
-      {comments && comments.length > 0 ? (
-        <div className="relative mt-9 pb-12">
-          <Carousel
-            responsive={responsive}
-            arrows={false}
-            swipeable
-            draggable
-            infinite
-            containerClass="carousel-container"
-            renderDotsOutside
-            customDot={<CustomCarouselDot />}
-            showDots={COMMENTS_PER_PAGE < comments.length}
-          >
-            {splitArrayIntoChunks(comments, COMMENTS_PER_PAGE).map(
-              (subArray, index) => (
-                <div key={index} className="grid grid-cols-3 gap-[1.375rem]">
-                  {subArray.map(({ id, src }) => (
-                    <div key={id} className="relative">
-                      <Image
-                        src={src}
-                        alt=""
-                        width="0"
-                        height="0"
-                        sizes="100vw"
-                        className="h-auto w-full rounded-[3.75rem]"
-                      />
-                    </div>
-                  ))}
-                </div>
-              )
-            )}
-          </Carousel>
-        </div>
-      ) : null}
-    </div>
+      <div className="relative mt-9 pb-12">
+        <Carousel
+          responsive={{
+            desktop: {
+              breakpoint: { max: 3000, min: 1024 },
+              items: 3
+            },
+            tablet: {
+              breakpoint: { max: 1024, min: 464 },
+              items: 2
+            },
+            mobile: { breakpoint: { max: 464, min: 0 }, items: 1 }
+          }}
+          arrows={false}
+          swipeable
+          draggable
+          infinite
+          className="m-auto"
+          containerClass="container"
+          renderDotsOutside
+          showDots
+        >
+          {commentPairs.map(comments => (
+            <div key={comments[0]} className="flex flex-col gap-4 p-2">
+              {comments.map(src => (
+                <Image
+                  key={src}
+                  src={src}
+                  alt=""
+                  width="0"
+                  height="0"
+                  sizes="100vw"
+                  className="h-auto w-full rounded-3xl"
+                />
+              ))}
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </SectionContainer>
   );
 }
